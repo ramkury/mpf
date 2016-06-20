0ad6eb46a482d383fca1ce71f84f8e2ca3527500
ramkury <ricardokury@aluno.unb.br>
2016-06-20 15:46:07 -0300

    Adiciona comentários nas últimas funções de GraphicalUserInterface.h

--- include/GraphicalUserInterface.h
+++ include/GraphicalUserInterface.h
@@ -68,30 +68,112 @@ WINDOW* UI_CriaJanelaEntrada(const char *szTitulo, TS_cores colorPair);
  */
 tpElementoGrafo* UI_ListaTarefas(pGrafo grafo);
 
-void UI_ImprimeTarefa(WINDOW *hwndLista, tpElementoGrafo* tarefa, int coluna, bool selecionado);
+/**
+ * Imprime os dados relativos a uma tarefa na linha e janela escolhidas
+ * @param hwndLista janela em que será impressa a tarefa
+ * @param tarefa Tarefa a ser impressa na tela
+ * @param linha Linha em que será impressa a tarefa
+ * @param selecionado Define se o cursor está posicionado na linha dessa tarefa
+ */
+void UI_ImprimeTarefa(WINDOW *hwndLista, tpElementoGrafo* tarefa, int linha, bool selecionado);
 
+/**
+ * Pede ao usuário informações necessárias para criação de uma nova tarefa,
+ * cria o objeto de tarefa com as informações obtidas do usuário e insere
+ * a tarefa no grafo.
+ * @param pCabeca ponteiro para a cabeça do grafo no qual a tarefa será inserida
+ */
 void UI_NovaTarefa(pGrafo pCabeca);
 
+/**
+ * Mostra um menu para o usuário com todos os dados da tarefa que podem ser alterados
+ * e chama a função de edição do atributo selecionado.
+ * @param grafo Grafo contendo a tarefa que deve ser editada
+ * @param tarefa Tarefa que está sendo editada
+ */
 void UI_EditarTarefa(pGrafo grafo, tpElementoGrafo *tarefa);
 
+/**
+ * Abre uma janela de entrada de texto e pede que o usuário digite o novo ID da tarefa.
+ * Se possível, atualiza o ID da tarefa. Se não, mostra mensagem de erro ao usuário.
+ * @param grafo Grafo contendo a tarefa a ser editada
+ * @param tarefa Tarefa a ser editada
+ */
 void UI_EditarID(pGrafo grafo, tpElementoGrafo *tarefa);
 
+/**
+ * Abre uma janela de entrada de texto e pede que o usuário digite o novo nome da tarefa.
+ * Se possível, atualiza o nome da tarefa. Se não, mostra mensagem de erro ao usuário.
+ * @param grafo Grafo contendo a tarefa a ser editada
+ * @param tarefa Tarefa a ser editada
+ */
 void UI_EditarNome(pGrafo grafo, tpElementoGrafo *tarefa);
 
+/**
+ * Abre uma janela de entrada de texto e pede que o usuário digite o novo tempo 
+ * mínimo de início  da tarefa.
+ * Se possível, atualiza o atributo da tarefa. Se não, mostra mensagem de erro ao usuário.
+ * @param grafo Grafo contendo a tarefa a ser editada
+ * @param tarefa Tarefa a ser editada
+ */
 void UI_EditarInicMin(pGrafo grafo, tpElementoGrafo *tarefa);
 
+/**
+ * Abre uma janela de entrada de texto e pede que o usuário digite o novo tempo 
+ * de duração da tarefa
+ * Se possível, atualiza o atributo da tarefa. Se não, mostra mensagem de erro ao usuário.
+ * @param grafo Grafo contendo a tarefa a ser editada
+ * @param tarefa Tarefa a ser editada
+ */
 void UI_EditarDuracao(pGrafo grafo, tpElementoGrafo *tarefa);
 
+/**
+ * Abre uma janela de entrada de texto e pede que o usuário digite o novo estado
+ * de execução da tarefa
+ * Se possível, atualiza o atributo da tarefa. Se não, mostra mensagem de erro ao usuário.
+ * @param grafo Grafo contendo a tarefa a ser editada
+ * @param tarefa Tarefa a ser editada
+ */
 void UI_EditarEstadoExec(pGrafo grafo, tpElementoGrafo *tarefa);
 
+/**
+ * Abre uma janela de entrada de texto e pede que o usuário digite o ID da tarefa a ser
+ * adicionada como pré requisito. Se possível, adiciona a tarefa na lista de pré requisitos.
+ * Se não, mostra mensagem de erro ao usuário.
+ * @param grafo Grafo contendo a tarefa a ser editada
+ * @param tarefa Tarefa a ser editada
+ */
 void UI_CriarRequisito(pGrafo grafo, tpElementoGrafo *tarefa);
 
+/**
+ * Abre uma janela de entrada de texto e pede que o usuário digite o ID da tarefa a ser
+ * removida da lista de pré requisitos. Se possível, remove a tarefa da lista de pré requisitos.
+ * Se não, mostra mensagem de erro ao usuário.
+ * @param grafo Grafo contendo a tarefa a ser editada
+ * @param tarefa Tarefa a ser editada
+ */
 void UI_ExcluirRequisito(pGrafo grafo, tpElementoGrafo *tarefa);
 
+/**
+ * Exclui a tarefa escolhida do grafo.
+ * @param grafo Grafo contendo a tarefa a ser excluída
+ * @param tarefa Tarefa a ser excluída
+ */
 void UI_ExcluirTarefa(pGrafo grafo, tpElementoGrafo *tarefa);
 
+/**
+ * Abre uma janela de entrada de texto e pede que o usuário digite o nome do arquivo
+ * de texto a ser criado com as informações do grafo.
+ * @param grafo Grafo a ser escrito no arquivo
+ */
 void UI_EscreverGrafo(pGrafo grafo);
 
+/**
+ * Abre uma janela de entrada de texto e pede que o usuário digite o instante de tempo
+ * para o qual o estado das tarefas deve ser calculado. Computa e altera o estado de execução
+ * de todas as tarefas do grafo para o tempo escolhido.
+ * @param grafo Grafo a ser atualizado de acordo com o tempo escolhido
+ */
 void UI_DefinirTempo(pGrafo grafo);
 
 #endif
